eb357dc317a03dcaa9d216564d917503dc6c6367
unknown <agnialung@gmail.com>
2024-08-30 17:34:12 +0700

    coba cek lagi

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -11,8 +11,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
+  headers: {"Access-Control-Allow-Origin": "*"},
   baseURL: 'https://0c4c-103-171-153-170.ngrok-free.app/', // URL ngrok publik untuk back end
   withCredentials: true,
 });
+
 
 export default instance;
