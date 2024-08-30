a7d4e48643e6c9eab19e6fa0ae864a7a3b939a27
unknown <agnialung@gmail.com>
2024-08-30 17:02:05 +0700

    update database

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,20 @@
+// import axios from 'axios';
+
+// const instance = axios.create({
+//   baseURL: 'http://127.0.0.1:5000',
+//   withCredentials: true, // untuk mengirimkan cookies/sessions
+// });
+
+// export default instance;
+
+
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
-  withCredentials: true, // untuk mengirimkan cookies/sessions
+  baseURL: 'https://0c4c-103-171-153-170.ngrok-free.app/', // URL ngrok publik untuk back end
+  withCredentials: true,
 });
 
 export default instance;
+
+
